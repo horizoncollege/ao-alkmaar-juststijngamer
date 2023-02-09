112174bb45b1280c72c2e7169f53000b80d10efe
stijn leegwater <168765@student.horizoncollege.nl>
2023-02-09 13:52:36 +0100

    start

---
+++ aplications/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Aplication dashboard</title>
+</head>
+
+<body>
+    <div class="index">
+        <h1>Aplication dashboard</h1>
+        <p>dit is mijn dashboard van mijn aplicaties</p>
+    </div>
+    <div class="body">
+        <a href="wie-ben-ik/" target="blank"> <img src="afbeeldingen/aboutme.jpg.crdownload" alt="about-me"></a>
+        <a href="galgje/" target="blank"> <img src="afbeeldingen/galgje.jpg" alt="galgje"></a>
+    </div>
+    <div class="body2">
+        <a href="rock/" target="blank"> <img src="afbeeldingen/rock.webp" alt="rock"></a>
+    </div>
+
+</body>
+
+</html>
