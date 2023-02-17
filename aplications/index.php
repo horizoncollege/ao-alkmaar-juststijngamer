38b949888409dcd2a4f8b2f7362f6c84d7ad6040
stijn leegwater <168765@student.horizoncollege.nl>
2023-02-17 09:30:27 +0100

    update

--- aplications/index.php
+++ aplications/index.php
@@ -1,27 +1,22 @@
 <!DOCTYPE html>
-<html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="style.css">
-    <title>Aplication dashboard</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <title>Application dashboard</title>
 </head>
 
 <body>
     <div class="index">
         <h1>Aplication dashboard</h1>
-        <p>dit is mijn dashboard van mijn aplicaties</p>
+        <p>Hier staan mijn applicaties die ik tijdens mijn opleiding software developer heb gemaakt</p>
     </div>
-    <div class="body">
-        <a href="wie-ben-ik/" target="blank"> <img src="afbeeldingen/aboutme.jpg.crdownload" alt="about-me"></a>
-        <a href="galgje/" target="blank"> <img src="afbeeldingen/galgje.jpg" alt="galgje"></a>
+    <div class="content">
+        <a href="wie-ben-ik/" target="blank"><img src="afbeeldingen/aboutme.jpg.crdownload" class="img-fluid" alt="me"> </a>
+        <a href="galgje/" target="blank"><img src="afbeeldingen/galgje.jpg" class="img-fluid" alt="galgje"> </a>
+        <a href="rock/" target="blank"><img src="afbeeldingen/rock.webp" class="img-fluid" alt="rock"> </a>
     </div>
-    <div class="body2">
-        <a href="rock/" target="blank"> <img src="afbeeldingen/rock.webp" alt="rock"></a>
-    </div>
-
 </body>
 
 </html>
